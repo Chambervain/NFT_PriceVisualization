a52ee05d72de1e83d3b9a33787259f61295d8fc9
Bowen Zhang <bz2306@nyu.edu>
2023-07-11 12:52:08 -0400

    Add functionality of searching nfts and display items on webpage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
+import { Layout, Input, Button, message, List, Card, Image } from "antd";
 import { searchNFTs } from "./utils";
+import { useState } from "react";
 
-const handleButtonClick = () => {
-  searchNFTs("car");
-};
+// JSON.stringify() -> flatten the JS object to json string
+// JSON.parse() -> deserialize json string to JS object
+
+//React function component - Hooks
+//const [state, setState function] = useState(default value)
+
+const { Header, Content } = Layout;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [nfts, setNfts] = useState([]);
+
+  const handleSearch = async () => {
+    setLoading(true);
+
+    try {
+      const data = await searchNFTs(searchText);
+      setNfts(data.result);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <Button onClick={handleButtonClick}>Test</Button>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+          NFT Browser
+        </div>
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", padding: 20, overflow: "auto" }}
+      >
+        <Input.Group compact style={{ width: 600 }}>
+          <Input
+            style={{
+              width: "calc(100% - 200px)",
+            }}
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <Button type="primary" onClick={handleSearch} loading={loading}>
+            Submit
+          </Button>
+        </Input.Group>
+        <br />
+        <List
+          grid={{ gutter: 16, column: 4 }}
+          dataSource={nfts}
+          renderItem={(item) => (
+            <List.Item>
+              <Card title={JSON.parse(item.metadata).name}>
+                <Image src={JSON.parse(item.metadata).image} />
+              </Card>
+            </List.Item>
+          )}
+        />
+      </Content>
+    </Layout>
   );
 }
 
